bad6a48d823c65060a4adfe9f08c2ab396008bc6
joao-ressel <joao.resselti@gmail.com>
2024-05-27 10:45:04 -0500

    feat: iniciando estilização do header e layout

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import { HeaderContainer } from "./styles";
+import logoignite from "../../assets/ignite-logo.svg";
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+export function Header() {
+  return (
+    <HeaderContainer>
+      <img src={logoignite} alt="" />
+      <nav>
+        <NavLink to="/">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
+}
