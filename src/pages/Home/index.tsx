8557550d21af45367642ac00f7f05ab978aeb651
joao-ressel <joao.resselti@gmail.com>
2024-05-27 11:53:11 -0500

    refact: fazendo a home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,44 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+
+export function Home() {
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput id="task" placeholder="Dê um nome para o seu projeto" />
+
+          <label htmlFor="minutes">dutante</label>
+          <MinutesAmountInput
+            placeholder="00"
+            type="minutes"
+            id="minutesAmount"
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <StartCountdownButton>
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
+}
