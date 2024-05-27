b26b679a98f08e1ad8023cd70508c103bb9c3136
joao-ressel <joao.resselti@gmail.com>
2024-05-27 08:59:30 -0500

    first commit

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,4 @@
+import "styled-components";
+import { defaultTheme } from "../styles/themes/defalt";
+
+type ThemeType = typeof defaultTheme;
---
+++ src/components/Button.styles.ts
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export type ButtonVariant = "primary" | "secondary" | "danger" | "success";
+
+interface ButtonContainerProps {
+  variant: ButtonVariant;
+}
+
+const buttonVariants = {
+  primary: "purple",
+  secondary: "orange",
+  danger: "red",
+  success: "green",
+};
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100px;
+  height: 40px;
+
+  background-color: ${props => props.theme.primary};
+  color: ${props => props.theme.white};
+  border: 0;
+  border-radius: 4px;
+  margin: 8px;
+
+  /* ${(props) => {
+    return `background-color: ${buttonVariants[props.variant]}`;
+  }} */
+`;
